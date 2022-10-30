04011e14e86be00d91bd4cfd4f65a883308019f6
DenLeon <leonden0046@gmail.com>
2022-10-30 23:26:15 +0200

    Add project files.

---
+++ Laba2/Laba2.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+double setValue(const char v[]);
+
+int main()
+{
+    double epsula;
+    double x;
+
+    epsula = setValue("epsula");
+
+    do
+    {
+        cout << "Please, enter x = ";
+        cin >> x;
+    } while (x <= 0.0 || x == 4.0);
+
+    double t = sqrt(x) / 2;
+    double y = 4 / (4 - x);
+    double func = y * (1 + t * sqrt(y) * asin(t));
+
+    unsigned int i = 0;
+    double a = 1;
+    double halfX = x / 2;
+
+    func -= a;
+
+    if (abs(func) >= epsula)
+    {
+        cout << "i = " << i << endl;
+        return 0;
+    }
+
+    //while (abs(func) < epsula)
+    //{
+    //    /*if (abs(func) >= epsula)
+    //        break;*/
+    //    i++;
+    //    a = a * halfX * i / (2 * i - 1);
+    //    func = func - a;
+    //    cout << "i = " << i << endl;
+    //}
+
+    do
+    {
+        i++;
+        a = a * halfX * i / (2 * i - 1);
+        func -= a;
+        cout << "i = " << i << endl;
+    } while (abs(func) < epsula);
+}
+
+double setValue(const char v[])
+{
+    int error;
+    double value;
+    do
+    {
+        error = 0;
+        cout << "Please enter " << v << " = ";
+        cin >> value;
+        if (cin.fail())
+        {
+            cout.clear();
+            cout << "Please enter a valid number" << endl;
+            error = 1;
+            cin.clear();
+            cin.ignore(80, '\n');
+        }
+    } while (error == 1);
+
+    return value;
+}
+
